81af7c35699ca18cc3087227e4a1141e91b40686
Lior Baraban <barabanlior@gmail.com>
2018-12-26 14:30:37 +0200

    Infra - Heroku config ongoing

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/vortex'));
+app.use(express.static(__dirname + '/dist/vortex-ios-pwa'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/vortex/index.html'));
+res.sendFile(path.join(__dirname+'/dist/vortex-ios-pwa/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
